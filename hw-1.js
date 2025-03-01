06942e76c081c5b345dc8888adb9e7af51720896
ExperementalStas <stas.1303@icloud.com>
2025-03-01 19:21:48 +0300

    1

---
+++ hw-1.js
@@ -0,0 +1,59 @@
+//Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2
+const iphone = 2007;
+iphone = alert("год рождения Iphone 2007");
+
+//Задание 3
+const creatjs = 3;
+creatjs = alert("Brendan Eich имя создателя JavaScript");
+
+//Задание 4
+let a1 = 10;
+let b1 = 2;
+alert(a1 + b1);
+alert(a1 - b1);
+alert(a1 * b1);
+alert(a1 / b1);
+
+//Задание 5
+let result = 2 ** 5;
+alert(result);
+
+//Задание 6
+let a2 = 9;
+let b2 = 2;
+alert(a2 % b2);
+ 
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//Задание 9
+const user = {
+  name: "Stan",
+  age: "35",
+  isAdmin: "true",
+};
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin);
+
+//Задание 10
+let name1 = prompt("Как вас зовут?");
+let e = "Привет ";
+alert(e + name1);
