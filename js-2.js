86a79f105e90411813c2798be027de016d6204de
ExperementalStas <stas.1303@icloud.com>
2025-03-10 19:21:31 +0300

    начал js2 и правка адаптации css mobi

---
+++ js-2.js
@@ -0,0 +1,17 @@
+//Задание 1
+let password = "444";
+let userInput = prompt("Введите пароль");
+if (userInput === password) {
+  alert("Пароль введен верно");
+} else {
+ alert("Пароль введен неправильно");
+}
+
+//Задание 2
+let x = 7;
+
+if (x > 0 && x < 10) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
