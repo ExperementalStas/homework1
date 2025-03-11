f33d8ca6296a424a87a6979c653a43aaa9a64f63
ExperementalStas <stas.1303@icloud.com>
2025-03-11 19:43:18 +0300

    создал js-3 файл и дз

---
+++ js-3.js
@@ -0,0 +1,37 @@
+// Задание 1
+console.log("Привет");
+console.log("Привет");
+
+// Задание 2
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// Задание 3
+for (let r = 7; r <= 22; r++) {
+  console.log(r);
+}
+// Задание 4
+let obj = {
+  Коля: "200",
+  Вася: "300",
+  Петя: "400",
+};
+
+for (let name in obj) {
+  console.log(`${name} — зарплата ${obj[name]} долларов`);
+}
+
+// Задание 5
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+  n = n / 2;
+  num++;
+}
+
+console.log(`Результат деления: ${n}`);
+console.log(`Количество итераций: ${num}`);
+
+
